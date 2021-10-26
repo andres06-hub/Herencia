b442d1e824447c0796923ee937c4fd0242b55e27
AndresSierra <andressierrarojas7@gmail.com>
2021-10-26 07:13:29 -0500

    Se termino la encapsulación

--- src/domain/Empleado.java
+++ src/domain/Empleado.java
@@ -7,5 +7,23 @@ public class Empleado extends Persona{
     private String fechaIngreso;
     private String areaCargo;
 
+    // 1)
+    public String getCodigoEmpleado(){return this.codigoEmpleado;}
+
+    public void setCodigoEmpleado(String codigoEmpleqado){this.codigoEmpleado = codigoEmpleqado;}
+
+    // 2)
+    public double getNumHorasExtra(){return this.numHorasExtras;}
     
+    public void setNumHorasExtras(double numHorasExtras){this.numHorasExtras = numHorasExtras;}
+ 
+    // 3)
+    public String getFechaIngreso(){return this.fechaIngreso;}
+
+    public void setFechaIngreso(String fechaIngreso){this.fechaIngreso = fechaIngreso;}
+
+    // 4)
+    public String getAreaCargo(){return this.areaCargo;}
+
+    public void setAreaCargo(String areaCargo){this.areaCargo = areaCargo;}
 }
